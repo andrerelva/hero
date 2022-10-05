da4bdcc83b723188bda48734d2043b94ca2d3780
André Relva <up202108695@up.pt>
2022-10-05 17:41:06 +0100

    Game creation

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,25 +3,41 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.virtual.DefaultVirtualTerminal;
 
 import java.io.IOException;
 
-public class Application {
-    public static void main(String[] args) {
+public class Game {
+    Screen screen;
+
+    public Game() {
         try {
             Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
+            screen = new TerminalScreen(terminal);
 
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() {
+        try {
+            draw();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.run();
     }
 }
