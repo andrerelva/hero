557d80c1a6639657fcdcee5cf3c5dd0955f9b365
André Relva <up202108695@up.pt>
2022-10-05 18:19:14 +0100

    Position class implemented

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -2,48 +2,49 @@ import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.screen.Screen;
 
 public class Hero {
-    private int x;
-    private int y;
+    private Position position;
 
     public Hero(int x, int y) {
-        this.x = x;
-        this.y = y;
+        position = new Position(x, y);
     }
 
     public int getX() {
-        return x;
+        return position.getX();
     }
 
     public int getY() {
-        return y;
+        return position.getY();
     }
 
     public void setX(int x) {
-        this.x = x;
+        position.setX(x);
     }
 
     public void setY(int y) {
-        this.y = y;
+        position.setY(y);
     }
 
     public void draw(Screen screen) {
-        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(position.getX(), position.getY(), TextCharacter.fromCharacter('X')[0]);
     }
 
-    public void moveUp() {
-        y--;
+    public Position moveUp() {
+        return new Position(position.getX(), position.getY() - 1);
     }
 
-    public void moveDown() {
-        y++;
+    public Position moveDown() {
+        return new Position(position.getX(), position.getY() + 1);
     }
 
-    public void moveLeft() {
-        x--;
+    public Position moveLeft() {
+        return new Position(position.getX() - 1, position.getY());
     }
 
-    public void moveRight() {
-        x++;
+    public Position moveRight() {
+        return new Position(position.getX() + 1, position.getY());
     }
 
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
