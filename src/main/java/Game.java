2b55e40f46e532e9f04a3bed967a2bbd1e133016
André Relva <up202108695@up.pt>
2022-10-05 17:59:19 +0100

    Moving key and quittable game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,18 @@
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
+import java.security.Key;
 
 public class Game {
-    Screen screen;
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     public Game() {
         try {
@@ -24,15 +29,42 @@ public class Game {
 
     private void draw() throws IOException {
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
     }
 
     public void run() {
-        try {
-            draw();
-        } catch (IOException e) {
-            e.printStackTrace();
+        while (true) {
+            try {
+                draw();
+                KeyStroke key = screen.readInput();
+                processKey(key);
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                    screen.close();
+                } else if (key.getKeyType() == KeyType.EOF) break;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void processKey(KeyStroke key) throws IOException {
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                y--;
+                break;
+            case ArrowDown:
+                y++;
+                break;
+            case ArrowLeft:
+                x--;
+                break;
+            case ArrowRight:
+                x++;
+                break;
+            case EOF:
+                break;
+
         }
     }
 
